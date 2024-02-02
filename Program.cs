bb75b76c2e0b80e2b0ffb33b8d5e4bc7d9c1ed9e
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-02 03:11:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericStacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var countries = new Stack<string>();
+
+            countries.Push("Switzerland");
+            countries.Push("Germany");
+            countries.Push("Dubai");
+            countries.Push("United States of America");
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+            }
+
+            var previouslyListedCountry = countries.Peek();
+            Console.WriteLine($"The previously visited country was: {previouslyListedCountry}");
+
+            var lastCountry = countries.Pop();
+
+            Console.WriteLine($"Hello, {lastCountry}");
+
+        }
+    }
+}
